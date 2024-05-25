0b1536f812e5e159a027783c3ab1c5ce7d5b4b2e
NicolasVyatkin <125182420+NicolasVyatkin@users.noreply.github.com>
2024-05-25 16:08:59 +0300

    Uploading completed java course

---
+++ MOD 3 JavaPro(Alexander+Vsevolod)/+HWMod1StartLes2BasicEx1TriangleSquare/+HWLes11JavaBot/src/main/java/academy/prog/javaprobot/JavaProBotApplication.java
@@ -0,0 +1,20 @@
+package academy.prog.javaprobot;
+/*
+*1.	Додати в роботу команду “видалити свій обліковий запис”
+*2.	Додати адмінську команду “зробити користувача адміном”
+*3.	Зробити так, щоб при першому приверненні бот відправляв користувачеві якусь прикольну картинку)
+*
+* */
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProBotApplication.class, args);
+    }
+
+}
