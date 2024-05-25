518e39ef3faa2c13c03ae3e859a4187af6299ebe
NicolasVyatkin <125182420+NicolasVyatkin@users.noreply.github.com>
2024-05-25 16:14:30 +0300

    Delete +HWLes9Task1HappyURL/HappyURL directory

--- +HWLes9Task1HappyURL/HappyURL/src/main/java/academy/prog/UrlRepository.java
+++
@@ -1,7 +0,0 @@
-package academy.prog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrlRepository extends JpaRepository<UrlRecord, Long> {
-    UrlRecord findByUrl(String url);
-}
